af98c71e2bdff8e7c8375c2150f294c981cd15e6
gabrielkey <gabrielgoldsa@gmail.com>
2023-12-27 18:15:16 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Classe herói
+
+Um algoritmo simples que cria uma classe com os atributos de um herói.
+## Lista de Feitiços
+
+* guerreiro: "espada"
+* mago: "magia"
+* monge: "artes marciais"
+* ninja: "shuriken"
---
+++ index.js
@@ -0,0 +1,30 @@
+/* guerreiro, mago, monge, ninja */ 
+
+class Heroi {
+    constructor(nome = "Desconhecido", idade = null, tipo = "Desconhecido"){
+        this.nome = nome;
+        this.idade = idade;
+        this.tipo = tipo;
+    }
+
+    ataque() {
+        const ataques = {
+            guerreiro: "espada",
+            mago: "magia",
+            monge: "artes marciais",
+            ninja: "shuriken"
+        };
+        const ataqueHeroi = ataques[this.tipo];
+
+        if (ataqueHeroi) {
+            return `O herói ${this.nome} do ${this.tipo} atacou usando ${ataqueHeroi}`;
+        } else {
+            return "Magia falhou!";
+        }
+    }
+    
+}
+
+let novoHeroi = new Heroi("Boni Souza", 17, "mago");
+
+console.log(novoHeroi.ataque());
